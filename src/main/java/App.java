4857b3100eccb3e87a8c048190d496d86ffe7ac2
Jenya559 <bankok96@gmail.com>
2023-01-13 17:15:29 +0300

    ver.1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,22 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld tree = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(tree.getMessage());
+        HelloWorld leaf = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println("Переменные ссылаются на один и тот же объект?"+(tree==leaf));
+        System.out.println(tree);
+        System.out.println(leaf);
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Cat one = context.getBean("myCat", Cat.class);
+        Cat two = context.getBean("myCat", Cat.class);
+        System.out.println("Переменные ссылаются на один и тот же объект?" + (one == two));
+        System.out.println(one);
+        System.out.println(two);
+
     }
 }
